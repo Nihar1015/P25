dc73453d6f40891425036f53f1588c1fadd2574e
Nihar1015 <69799996+Nihar1015@users.noreply.github.com>
2020-11-18 16:11:36 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -5,18 +5,20 @@ class Paper{
            restitution: 0.3,
            friction: 0.5,
            density: 1.2
-           this.image = loadImage("Images/paper.png");
+           
 
         }
         this.body = Bodies.circle(x,y,r, options);
         this.radius = r;
+        this.image = loadImage("Images/paper.png");
 
         World.add(world, this.body);
+        
     }
        display(){
            var pos = this.body.position
-           ellipseMode(RADIUS);
-           ellipse(pos.x, pos.y, this.radius);
+           imageMode(CENTER);
+           image(this.image, 0,0, this.radius);
        }
 
     }
